1bd78141c9b14e44374f7601dd4a82f3c60810b2
Aswin CJ <aswincj4@gmail.com>
2020-02-16 12:55:02 +0530

    Restructing to support future tools

---
+++ src/Components/MainTools/Panther.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Panther() {
+  return (
+    <div className="Panther" style={{ width: "0px", height: "0px" }}>
+      <h1>Panther is Born</h1>
+    </div>
+  );
+}
+
+export default Panther;
