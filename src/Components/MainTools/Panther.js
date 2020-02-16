33b43b4d9a86a6b5b1bfaf007de1a34f2481854d
Aswin CJ <aswincj4@gmail.com>
2020-02-16 13:10:08 +0530

    Ported To RiafyTools Repo

--- src/Components/MainTools/Panther.js
+++ src/Components/MainTools/Panther.js
@@ -4,6 +4,7 @@ function Panther() {
   return (
     <div className="Panther" style={{ width: "0px", height: "0px" }}>
       <h1>Panther is Born</h1>
+      <h1>Ported To RiafyTools Repo</h1>
     </div>
   );
 }
