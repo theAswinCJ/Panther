ccbe2ad1f981ddc65c89cd5274d70f0d4af568c7
Aswin CJ <aswincj4@gmail.com>
2020-02-16 12:37:00 +0530

    Updates

---
+++ precache-manifest.8202de8717ff9344648ecc1841e4b1d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4855832c4e87a1fb58424bd18574a273",
+    "url": "/Panther/index.html"
+  },
+  {
+    "revision": "e785a38b7c119b269c9b",
+    "url": "/Panther/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0403b1c6282026924015",
+    "url": "/Panther/static/js/2.c18a5553.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Panther/static/js/2.c18a5553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e785a38b7c119b269c9b",
+    "url": "/Panther/static/js/main.4cd0c920.chunk.js"
+  },
+  {
+    "revision": "eebb54fbfa7ce327a125",
+    "url": "/Panther/static/js/runtime-main.ee56d1fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Panther/static/media/logo.5d5d9eef.svg"
+  }
+]);
