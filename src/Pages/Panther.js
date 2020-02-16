b04e4ee75d711d9eac4081a7d62621688668110d
Aswin CJ <aswincj4@gmail.com>
2020-02-16 20:07:34 +0530

    found a package that does something

--- src/Pages/Panther.js
+++ src/Pages/Panther.js
@@ -1,6 +1,17 @@
 import React from "react";
+import {
+  readSketchFile,
+  writeSketchFile,
+  createNewSketchFile,
+  generateId
+} from "sketch-file";
 
 function Panther() {
+  console.log("SketchFile readSketchFile", readSketchFile);
+  console.log("SketchFile writeSketchFile", writeSketchFile);
+  console.log("SketchFile createNewSketchFile", createNewSketchFile);
+  console.log("SketchFile generateId", generateId);
+
   return (
     <div className="Panther" style={{ width: "0px", height: "0px" }}>
       <h1>Panther is Born</h1>
