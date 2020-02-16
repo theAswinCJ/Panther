3a92cf1daad7cde8ecc95fc58e468737d811644e
Aswin CJ <aswincj4@gmail.com>
2020-02-16 13:04:07 +0530

    restructing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import Panther from "Components/MainTools/Panther.js";
+import Panther from "./Components/MainTools/Panther.js";
 
 function App() {
   return (
