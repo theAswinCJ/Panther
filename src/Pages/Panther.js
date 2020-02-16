5c2f782de0266319e63f34a856e7f8e77c5b7591
Aswin CJ <aswincj4@gmail.com>
2020-02-16 21:41:00 +0530

    Days end. conclusion -  browsers dont natively support fs - filesystem functions. the progress we made was using a sketch io helper that uses fs systems. so read and writing of sketch files didnt work. making it useless

--- src/Pages/Panther.js
+++ src/Pages/Panther.js
@@ -7,16 +7,35 @@ import {
 } from "sketch-file";
 
 function Panther() {
-  console.log("SketchFile readSketchFile", readSketchFile);
-  console.log("SketchFile writeSketchFile", writeSketchFile);
-  console.log("SketchFile createNewSketchFile", createNewSketchFile);
-  console.log("SketchFile generateId", generateId);
+  // VERIFY IF IMPORTS WORK
+  // console.log("SketchFile readSketchFile", readSketchFile);
+  // console.log("SketchFile writeSketchFile", writeSketchFile);
+  // console.log("SketchFile createNewSketchFile", createNewSketchFile);
+  // console.log("SketchFile generateId", generateId);
 
+  /*-------------------------------------------------------------------------------------------
+                        DOCUMENTATION
   // readSketchFile(filePath); 1 parameter
   // writeSketchFile(sketchFileToWrite, filePath); 2 parameters
   // createNewSketchFile(version) ; 1 parameter
   // generateId(); 0 parameters from what i saw
+  -------------------------------------------------------------------------------------------*/
 
+  // CREATE NEW FILE -> WORKS
+  let newPanther = createNewSketchFile(62);
+
+  // READ A FILE
+  // let readpath = "./panther.sketch";
+  // let newFile = readSketchFile(readpath);
+  // console.log("SketchFile panther", newFile);
+
+  // WRITE A FILE
+  let writepath = "./Newpanther2";
+  writeSketchFile(newPanther, writepath);
+  console.log("SketchFile panther written");
+
+  // GENERATE ID
+  // let newid = generateId();
   return (
     <div className="Panther" style={{ width: "0px", height: "0px" }}>
       <h1>Panther is Born</h1>
